69ff724b70734cd40db3ef062f26b8c146b53c49
Anders Lindahl <Anders@Anderss-MacBook-Pro.local>
2020-02-11 17:04:27 +0100

    funkar med midi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
 
-function kopplaTillNode() {
+const [access, setaccess] = useState(null)
+
+useEffect(() => {
+  navigator.requestMIDIAccess({sysex: true})
+  .then(function(access) {
+
+    setaccess(access)
+
+     access.onstatechange = function(e) {
+       // Print information about the (dis)connected MIDI controller
+       console.log(e);
+     };
+  });
+}, [])
+
+
+if(access) {
+  for (var input of access.inputs.values())
+      input.onmidimessage = getMIDIMessage;
+
+  function getMIDIMessage(midiMessage) {
+    console.log(midiMessage.data[0]);
+    kopplaTillNode(midiMessage.data['0'])
+  }
+}
+
+
+function kopplaTillNode(data) {
   fetch("http://192.168.1.104:4000/", {
     method: 'POST',
-    body: JSON.stringify({
-      text: "här kommer post, vi testar igen!!!"
-    }),
+    body: JSON.stringify({data}),
     headers: {
       'Content-Type': 'application/json'
     }
@@ -24,7 +49,7 @@ function kopplaTillNode() {
   return (
     <div>
       <h1>Hello World!!!</h1>
-      <button onClick={()=>kopplaTillNode()}>koppla till node</button>
+      <button onClick={()=>kopplaTillNode("från knappen")}>koppla till node</button>
     </div>
   )
 }
